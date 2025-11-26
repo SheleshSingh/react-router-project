fcc861474ef49e7a01772f96744585cec708dce6
Shelesh RDX <sheleshsingh755@gmail.com>
2025-11-26 14:58:01 +0530

    added the suspense

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 
 const MainLayout = () => {
   return (
@@ -9,7 +10,9 @@ const MainLayout = () => {
         <Header />
       </div>
       <div className="h-[80%] bg-blue-600 text-white flex flex-col justify-center items-center text-2xl gap-3">
-        <Outlet />
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <Outlet />
+        </Suspense>
       </div>
       <div className="h-[10%] bg-gray-600 text-white  text-center text-5xl">
         <Footer />
